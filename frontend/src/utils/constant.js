205a7a1157c756656e5b7eeba618f57362c4becf
Deepak singh <130153943+sdeepak83@users.noreply.github.com>
2024-07-28 15:08:15 +0530

    Update constant.js

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,7 +1,6 @@
 
 
 export const USER_END_POINT = "http://localhost:8000/api/v1/user";
-htt
 export const JOB_API_END_POINT = "http://localhost:8000/api/v1/job";
 
 export const APPLICATION_API_END_POINT =
