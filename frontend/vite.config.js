5e60f375fa07bb730ca6ad5d7e9f409aa7c4293a
Deepak singh <130153943+sdeepak83@users.noreply.github.com>
2024-07-28 14:43:28 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,11 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
-  server:{
-    port:2000
-  }
+  
+  server: {
+    port: 2000,
+    // origin: "http://127.0.0.1:8080",
+  },
   headers: {
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
