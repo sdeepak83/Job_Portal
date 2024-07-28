89f2d88aba34414cc55fc3ff044ebadc88c7ef5a
Deepak singh <130153943+sdeepak83@users.noreply.github.com>
2024-07-28 14:36:27 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,10 +8,7 @@ export default defineConfig({
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
   },
-  server: {
-    port: 2000,
-    // origin: "http://127.0.0.1:8080",
-  },
+ 
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
