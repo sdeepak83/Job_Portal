215653d4fca538750a83f4be1966414149f726d1
Deepak singh <130153943+sdeepak83@users.noreply.github.com>
2024-07-28 13:56:03 +0530

    constant

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,8 +1,20 @@
-export const USER_END_POINT = "http://localhost:8000/api/v1/user";
+export const USER_END_POINT =
+  "https://job-portal-backend-vvjj.onrender.com/api/v1/user";
 
-export const JOB_API_END_POINT = "http://localhost:8000/api/v1/job";
+export const JOB_API_END_POINT =
+  "https://job-portal-backend-vvjj.onrender.com/api/v1/job";
 
 export const APPLICATION_API_END_POINT =
-  "http://localhost:8000/api/v1/application";
+  "https://job-portal-backend-vvjj.onrender.com/api/v1/application";
 
-export const COMPANY_API_END_POINT = "http://localhost:8000/api/v1/company";
+export const COMPANY_API_END_POINT =
+  "https://job-portal-backend-vvjj.onrender.com/api/v1/company";
+
+// export const USER_END_POINT = "p://localhost:8000/api/v1/user";
+// htt
+// export const JOB_API_END_POINT = "http://localhost:8000/api/v1/job";
+
+// export const APPLICATION_API_END_POINT =
+//   "http://localhost:8000/api/v1/application";
+
+// export const COMPANY_API_END_POINT = "http://localhost:8000/api/v1/company";
