3fcfed171c4c4fc6d0b7221308afb2223f589950
Deepak singh <130153943+sdeepak83@users.noreply.github.com>
2024-07-29 08:31:18 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,41 +16,16 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-// const corsOptions = {
-//   origin: "http//localhost:5173",
-//   Credentials: true,
-// };
 
-app.use(
-  cors({
-    credentials: false,
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
-    allowedHeaders: ["Content-Type", "Authorization"],
-    origin: [
-      "https://job-portal-frontend-4r7o.onrender.com",
-      
-      "http://localhost:8000",
-      "http://localhost:2000",
-      "http://localhost:4000",
-      "http://localhost:5173/",
-    ],
-  })
-);
+const corsOptions = {
+    origin:'https://job-portal-frontend-4r7o.onrender.com',
+    credentials:true
+}
+
+app.use(cors(corsOptions));
 
 
 
-
-// app.use(
-//   cors({
-//     origin: "",
-//     headers: {
-//       "Access-Control-Allow-Origin": "https://job-portal-frontend-4r7o.onrender.com",
-//       "Access-Control-Allow-Credentials": true,
-//     },
-//   })
-// );
-
-app.use(cors())
 const PORT = process.env.PORT || 3000;
 
 app.use("/api/v1/user", userRoute);
