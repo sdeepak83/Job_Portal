155143a7b2eb2b2c0d8e66049a747238a9c28b11
Deepak singh <130153943+sdeepak83@users.noreply.github.com>
2024-07-29 08:26:34 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,22 +1,12 @@
-import path from "path";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
-  
-  // server: {
-  //   // port: 2000,
-  //   // origin: "http://127.0.0.1:8080",
-  // },
-  // headers: {
-  //   "Access-Control-Allow-Origin": "",
-  //   "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
-  // },
- 
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+})
