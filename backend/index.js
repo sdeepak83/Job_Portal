48d66810d77499d519ce1cb957578a948bedb265
Deepak singh <130153943+sdeepak83@users.noreply.github.com>
2024-07-29 07:56:42 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -21,28 +21,31 @@ app.use(cookieParser());
 //   Credentials: true,
 // };
 
-// app.use(
-//   cors({
-//     credentials: true,
-//     methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
-//     allowedHeaders: ["Content-Type", "Authorization"],
-//     origin: [
-//       "http://localhost:2000",
-//       "http://localhost:4000",
-//       "http://localhost:5173/",
-//     ],
-//   })
-// );
-
 app.use(
   cors({
-    origin: "https://job-portal-frontend-4r7o.onrender.com",
-    headers: {
-      "Access-Control-Allow-Origin": "https://job-portal-frontend-4r7o.onrender.com",
-      "Access-Control-Allow-Credentials": true,
-    },
+    credentials: true,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    allowedHeaders: ["Content-Type", "Authorization"],
+    origin: [
+      "https://job-portal-frontend-4r7o.onrender.com",
+      
+      "http://localhost:8000",
+      "http://localhost:2000",
+      "http://localhost:4000",
+      "http://localhost:5173/",
+    ],
   })
 );
+
+// app.use(
+//   cors({
+//     origin: "",
+//     headers: {
+//       "Access-Control-Allow-Origin": "https://job-portal-frontend-4r7o.onrender.com",
+//       "Access-Control-Allow-Credentials": true,
+//     },
+//   })
+// );
 
 app.use(cors())
 const PORT = process.env.PORT || 3000;
