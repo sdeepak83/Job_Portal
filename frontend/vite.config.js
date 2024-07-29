3e187f570856b13af42400e900a9500e24318300
Deepak singh <130153943+sdeepak83@users.noreply.github.com>
2024-07-29 08:20:36 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,14 +5,14 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [react()],
   
-  server: {
-    // port: 2000,
-    // origin: "http://127.0.0.1:8080",
-  },
-  headers: {
-    "Access-Control-Allow-Origin": "",
-    "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
-  },
+  // server: {
+  //   // port: 2000,
+  //   // origin: "http://127.0.0.1:8080",
+  // },
+  // headers: {
+  //   "Access-Control-Allow-Origin": "",
+  //   "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+  // },
  
   resolve: {
     alias: {
